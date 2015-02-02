8ae6dcebdcc1581ac50dd59abaa0959208a975b4
Marco Miglierina <marco.miglierina@icloud.com>
2015-02-02 11:20:30 +0100

    environment from Dockerfile and Vagrantfile

--- integration-tests/dda/startup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cd /opt/dda/rsp-services-csparql-0.4.6.2-modaclouds
-./rsp-services-csparql
--- integration-tests/kb/startup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cd /opt/fuseki/jena-fuseki-1.1.1/
-./fuseki-server --update --loc /data /modaclouds/kb
